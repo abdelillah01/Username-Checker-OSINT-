31ef23fe9a819b6d2eaeda0d77e39cffc7193a36
abdelillah <abdelillah.ouraou@gmail.com>
2025-08-14 20:09:48 +0100

    adding other platformes

--- platforms.py
+++ platforms.py
@@ -5,4 +5,11 @@ PLATFORMS = {
     "Instagram": "https://www.instagram.com/{}",
     "Twitter": "https://twitter.com/{}",
     "Reddit": "https://www.reddit.com/user/{}",
+    "YouTube": "https://www.youtube.com/@{}",
+    "TikTok": "https://www.tiktok.com/@{}",
+    "LinkedIn": "https://www.linkedin.com/in/{}",
+    "Pinterest": "https://www.pinterest.com/{}/",
+    "Twitch": "https://www.twitch.tv/{}",
+    "Snapchat": "https://www.snapchat.com/add/{}",
+    "WhatsApp": "https://wa.me/{}",
 }
